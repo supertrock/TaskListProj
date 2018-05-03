5f620eb1156ade3ff637f08977141f7077536762
supertrock <tmangramrock@gmail.com>
2018-05-02 23:43:30 -0400

    Add files via upload
    I edited the Tab Experiment from the practicals because of its use of the option menu and app bar. Then I adjusted some of the XML Layouts to execute some of the needs of the project. My challenges were updating the main classes

---
+++ main/java/com/example/tmangramrock/tabexperiment/TabFragment1.java
@@ -0,0 +1,21 @@
+package com.example.tmangramrock.tabexperiment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.ViewGroup;
+
+
+
+
+public class TabFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment1, container, false);
+    }
+
+
+}
---
+++ main/java/com/example/tmangramrock/tabexperiment/TabFragment3.java
@@ -0,0 +1,19 @@
+package com.example.tmangramrock.tabexperiment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_fragment3, container, false);
+    }
+
+}
